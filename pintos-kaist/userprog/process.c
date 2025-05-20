4c9709f7f803c73fdd7544ce7f0b93669b21e28b
goochul-im <goochul175465@gmail.com>
2025-05-20 13:31:20 +0900

    주석 제거

--- pintos-kaist/userprog/process.c
+++ pintos-kaist/userprog/process.c
@@ -194,10 +194,6 @@ __do_fork(void *aux)
 	 * TODO: 힌트) 파일 객체를 복제하려면 include/filesys/file.h의 `file_duplicate`를 사용하세요.
 	 * TODO:       이 함수가 부모의 자원을 성공적으로 복제할 때까지 부모는 fork()에서 반환되면 안 됩니다. */
 	/* 부모의 fd_table을 순회하며 복사 */
-	// if (parent->fd_table[0] != NULL)
-	// 	current->fd_table[0] = parent->fd_table[0]; // 표준 입력
-	// if (parent->fd_table[1] != NULL)
-	// 	current->fd_table[1] = parent->fd_table[1]; // 표준 출력
 	for (int i = 0; i < MAX_FD; i++)
 	{
 		if (parent->fd_table[i] != NULL)
